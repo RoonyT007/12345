7ab85e0c9fc48d82ae518694d851ab6650a8117d
RoonyT007 <josephsolmony29@gmail.com>
2024-07-28 00:19:04 +0530

    Revert "full screen and game mode change survey"
    This reverts commit 803ad7df99ab509d9ac7e182a9bd990a75567362.

--- build/indexmain.js
+++ build/indexmain.js
@@ -1,37 +1,3 @@
 window.addEventListener('load',()=>{
     document.getElementsByClassName('loader-container')[0].style.display='none';
-    
 })
-
-
-  document.addEventListener('keydown',function(event) {
-    if (event.key == 'F11') {
-        event.preventDefault();
-        fullScreenFunc(); // From fullscreen API
-    }
-})
- 
-
-function fullScreenFunc(){
-    if(document.fullscreen){
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.webkitExitFullscreen) { /* Safari */
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) { /* IE11 */
-        document.msExitFullscreen();
-      }
-      document.getElementById('full-screen-button').src='../svg/fullscreen.svg'
-    }
-    else{
-      if (document.getElementById('higher-container').requestFullscreen) {
-        document.getElementById('higher-container').requestFullscreen();
-      } else if (document.getElementById('higher-container').webkitRequestFullscreen) { /* Safari */
-        document.getElementById('higher-container').webkitRequestFullscreen();
-      } else if (document.getElementById('higher-container').msRequestFullscreen) { /* IE11 */
-        document.getElementById('higher-container').msRequestFullscreen();
-      }
-      document.getElementById('full-screen-button').src='../svg/exitfullscreen.svg'
-    }
-
-}
